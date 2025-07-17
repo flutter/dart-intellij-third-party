48ecd9dd2416ddf138d9c5c5d4922a9f22b233b2
Jaime Wren <jwren@google.com>
2025-07-17 10:45:45 -0700

    Progress on Gradle build.
    - build.gradle.kts changes, addition of `gen` directory (BNF output) into sourceSets
    - repositories addition
    - Windows-1252 addition
    - Addition of intellijIdeaCommunity version into intellijPlatform for current master on intellij-community, 253

--- third_party/build.gradle.kts
+++ third_party/build.gradle.kts
@@ -2,49 +2,29 @@ import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import org.jetbrains.intellij.platform.gradle.models.ProductRelease
 
-dependencies {
-    implementation(fileTree("lib") { include("*.jar") })
-}
-
-sourceSets {
-    main {
-        java {
-            srcDir("thirdPartySrc/analysisServer")
-            srcDir("thirdPartySrc/vmServiceDrivers")
-        }
+// Specify UTF-8 for all compilations so we avoid Windows-1252.
+allprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+    tasks.withType<Test> {
+        systemProperty("file.encoding", "UTF-8")
     }
 }
 
+// Plugins - must be first
 plugins {
     id("java") // Java support
     id("org.jetbrains.kotlin.jvm") version "2.1.21-RC2" // Kotlin support
-    id("org.jetbrains.intellij.platform") version "2.5.0" // IntelliJ Platform Gradle Plugin
-}
-
-// Set the JVM language level used to build the project.
-kotlin {
-    jvmToolchain(21)
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    id("org.jetbrains.intellij.platform") version "2.6.0" // IntelliJ Platform Gradle Plugin
 }
 
 // Configure project's dependencies
 repositories {
     mavenCentral()
-
     // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
     intellijPlatform {
         defaultRepositories()
-    }
-}
-
-dependencies {
-    intellijPlatform {
-        intellijIdeaCommunity("2024.3.1")
-        testFramework(TestFrameworkType.Platform)
     }
 }
 
@@ -56,6 +36,33 @@ intellijPlatform {
             untilBuild = "253.*"
         }
     }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("gen")
+            srcDir("thirdPartySrc/analysisServer")
+            srcDir("thirdPartySrc/vmServiceDrivers")
+        }
+    }
+}
+
+// Set the JVM language level used to build the project.
+kotlin {
+    jvmToolchain(21)
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.3.1")
+        testFramework(TestFrameworkType.Platform)
+        bundledModule("intellij.platform.coverage")
+    }
+    implementation(fileTree("lib") { include("*.jar") })
 }
 
 tasks {
