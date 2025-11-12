b8c3a477971955aea484979df81e0354aeb2a679
Helin Shiah <helinx@google.com>
2025-11-12 14:25:02 -0800

    Move service code to new class

--- third_party/src/main/java/com/jetbrains/lang/dart/ide/toolingDaemon/DartToolingDaemonService.kt
+++ third_party/src/main/java/com/jetbrains/lang/dart/ide/toolingDaemon/DartToolingDaemonService.kt
@@ -109,55 +109,7 @@ class DartToolingDaemonService private constructor(val project: Project, cs: Cor
       connectToDtdWebSocket(it)
       DartAnalysisServerService.getInstance(project).connectToDtd(uri)
     }
-    setUpEditorService()
-  }
-
-  private fun setUpEditorService() {
-    registerServiceMethod("Editor", "getActiveLocation", JsonObject()) {
-        val result = ReadAction.nonBlocking<JsonObject> {
-            getActiveLocation(project, this)
-        }.executeSynchronously()
-        result.addProperty("type", "ActiveLocation")
-
-        DartToolingDaemonResponse(result, null)
-    }
-
-    registerServiceMethod("Editor", "navigateToCode", JsonObject()) handler@{ request ->
-      val fileUri: String? = request.get("uri").asString
-      if (fileUri == null) {
-        val params = JsonObject()
-        params.addProperty("message", "No uri provided")
-        return@handler DartToolingDaemonResponse(null, params)
-      }
-
-      var path: String? = null
-      try {
-        path = URI(fileUri).toURL().file
-      } catch (e: MalformedURLException) {
-        // A null path will cause an early return.
-      } catch (e: URISyntaxException) {
-      }
-      if (path == null) {
-        val params = JsonObject()
-        params.addProperty("message", "Path could not be found from fileUri: $fileUri")
-        return@handler DartToolingDaemonResponse(null, params)
-      }
-
-      val file = LocalFileSystem.getInstance().findFileByPath(path)
-      val line: Int = request.get("line").asInt
-      val column: Int = request.get("column").asInt
-
-      ApplicationManager.getApplication().invokeLater(Runnable {
-        if (file != null && line >= 0 && column >= 0) {
-          val position = XSourcePositionImpl.create(file, line - 1, column - 1)
-          position.createNavigatable(project).navigate(false)
-        }
-      })
-
-      val params = JsonObject()
-      params.addProperty("success", true)
-      DartToolingDaemonResponse(params, null)
-    }
+    DtdEditorService(project, this).setUpService()
   }
 
   private fun connectToDtdWebSocket(uri: String) {
