7d7eb8e7cc40b8bbeb7a9433bb0b8a33079749f7
Helin Shiah <helinx@google.com>
2025-12-04 18:18:11 -0800

    Add PluginLogger for Dart

---
+++ third_party/src/main/java/com/jetbrains/lang/dart/logging/PluginLogger.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package com.jetbrains.lang.dart.logging
+
+import com.intellij.openapi.application.PathManager
+import java.io.File
+import java.io.IOException
+import java.util.logging.FileHandler
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
+
+object PluginLogger {
+  private const val LOG_FILE_NAME = "dash.log"
+
+  private val rootLogger: Logger = Logger.getLogger("com.jetbrains.lang.dart")
+
+  // This handler specifies the logging format and location.
+  private var fileHandler: FileHandler
+
+  init {
+    val logPath = PathManager.getLogPath()
+    try {
+      fileHandler = FileHandler(logPath + File.separatorChar + LOG_FILE_NAME, 1024 * 1024, 1)
+    } catch (e: IOException) {
+      throw RuntimeException(e)
+    }
+    System.setProperty(
+      "java.util.logging.SimpleFormatter.format",
+      "%1\$tF %1\$tT %3\$s [%4$-7s] %5\$s %6\$s %n"
+    )
+    fileHandler.formatter = SimpleFormatter()
+    rootLogger.addHandler(fileHandler)
+  }
+
+  fun createLogger(logClass: Class<*>): com.intellij.openapi.diagnostic.Logger {
+    return com.intellij.openapi.diagnostic.Logger.getInstance(logClass.getName())
+  }
+}
