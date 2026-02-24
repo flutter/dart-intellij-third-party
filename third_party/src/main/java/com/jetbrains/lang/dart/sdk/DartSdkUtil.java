3ebe5a4ffc0884c1e675414e6d780351783f02ae
Cory R <radcliffc@google.com>
2026-02-23 16:45:36 -0800

    Refactor: Update ComboBoxWithBrowseButton usage
    This commit updates the usage of ComboBoxWithBrowseButton to the new API. This fixes the compilation error that was occurring after the IntelliJ Platform SDK update.

--- third_party/src/main/java/com/jetbrains/lang/dart/sdk/DartSdkUtil.java
+++ third_party/src/main/java/com/jetbrains/lang/dart/sdk/DartSdkUtil.java
@@ -2,36 +2,26 @@
 package com.jetbrains.lang.dart.sdk;
 
 import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.ComponentWithBrowseButton;
-import com.intellij.openapi.ui.TextComponentAccessor;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.Strings;
-import com.intellij.ui.ComboboxWithBrowseButton;
-import com.intellij.ui.DocumentAdapter;
-import com.intellij.ui.components.JBLabel;
 import com.intellij.util.ArrayUtilRt;
 import com.intellij.util.SmartList;
-import com.intellij.util.system.CpuArch;
 import com.jetbrains.lang.dart.DartBundle;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.text.JTextComponent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 
 public final class DartSdkUtil {
   private static final Map<Pair<File, Long>, String> ourVersions = new HashMap<>();
@@ -63,65 +53,6 @@ public final class DartSdkUtil {
     return Strings.isNotEmpty(path) && new File(path + "/lib/core/core.dart").isFile();
   }
 
-  public static void initDartSdkControls(final @Nullable Project project,
-                                         final @NotNull ComboboxWithBrowseButton dartSdkPathComponent,
-                                         final @NotNull JBLabel versionLabel) {
-    dartSdkPathComponent.getComboBox().setEditable(true);
-    addKnownPathsToCombo(dartSdkPathComponent.getComboBox(), DART_SDK_KNOWN_PATHS, DartSdkUtil::isDartSdkHome);
-    if (getItemFromCombo(dartSdkPathComponent.getComboBox()).isEmpty()) {
-      // Suggest default path according to https://dart.dev/get-dart.
-      // No need to check folder presence here; even if it doesn't exist - that's the best we can suggest
-      @NlsSafe String path = SystemInfo.isMac ? CpuArch.isArm64() ? "/opt/homebrew/opt/dart/libexec"
-                                                                  : "/usr/local/opt/dart/libexec"
-                                              : SystemInfo.isWindows ? "C:\\tools\\dart-sdk"
-                                                                     : SystemInfo.isLinux ? "/usr/lib/dart"
-                                                                                          : null;
-      if (path != null) {
-        dartSdkPathComponent.getComboBox().getEditor().setItem(path);
-      }
-    }
-
-    final String sdkHomePath = getItemFromCombo(dartSdkPathComponent.getComboBox());
-    final String sdkVersion = getSdkVersion(sdkHomePath);
-    versionLabel.setText(sdkVersion == null ? "" : sdkVersion);
-
-    final TextComponentAccessor<JComboBox> textComponentAccessor = new TextComponentAccessor<>() {
-      @Override
-      public String getText(final JComboBox component) {
-        return getItemFromCombo(component);
-      }
-
-      @Override
-      public void setText(final @NotNull JComboBox component, final @NotNull String text) {
-        if (!text.isEmpty() && !isDartSdkHome(text)) {
-          final String probablySdkPath = text + "/dart-sdk";
-          if (isDartSdkHome(probablySdkPath)) {
-            component.getEditor().setItem(FileUtil.toSystemDependentName(probablySdkPath));
-            return;
-          }
-        }
-
-        component.getEditor().setItem(FileUtil.toSystemDependentName(text));
-      }
-    };
-
-    var descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor().withTitle(DartBundle.message("button.browse.dialog.title.select.dart.sdk.path"));
-    var browseFolderListener = new ComponentWithBrowseButton.BrowseFolderActionListener<>(dartSdkPathComponent, project, descriptor, textComponentAccessor);
-    dartSdkPathComponent.addActionListener(browseFolderListener);
-
-    final JTextComponent editorComponent = (JTextComponent)dartSdkPathComponent.getComboBox().getEditor().getEditorComponent();
-    editorComponent.getDocument().addDocumentListener(new DocumentAdapter() {
-      @Override
-      protected void textChanged(final @NotNull DocumentEvent e) {
-        final String sdkHomePath = getItemFromCombo(dartSdkPathComponent.getComboBox());
-        if(e.getType() == DocumentEvent.EventType.INSERT){
-          final String sdkVersion = getSdkVersion(sdkHomePath);
-          versionLabel.setText(sdkVersion == null ? "" : sdkVersion);
-        }
-      }
-    });
-  }
-
   private static @NotNull String getItemFromCombo(final @NotNull JComboBox combo) {
     return combo.getEditor().getItem().toString().trim();
   }
@@ -134,9 +65,7 @@ public final class DartSdkUtil {
     return null;
   }
 
-  private static void addKnownPathsToCombo(final @NotNull JComboBox combo,
-                                           final @NotNull String propertyKey,
-                                           final @NotNull Predicate<? super String> pathChecker) {
+  public static void addKnownSDKPathsToCombo(final @NotNull JComboBox<String> combo) {
     final SmartList<String> validPathsForUI = new SmartList<>();
 
     final String currentPath = getItemFromCombo(combo);
@@ -144,11 +73,11 @@ public final class DartSdkUtil {
       validPathsForUI.add(currentPath);
     }
 
-    List<String> knownPaths = PropertiesComponent.getInstance().getList(propertyKey);
+    List<String> knownPaths = PropertiesComponent.getInstance().getList(DART_SDK_KNOWN_PATHS);
     if (knownPaths != null && !knownPaths.isEmpty()) {
       for (String path : knownPaths) {
         final String pathSD = FileUtil.toSystemDependentName(path);
-        if (!pathSD.equals(currentPath) && pathChecker.test(path)) {
+        if (!pathSD.equals(currentPath) && isDartSdkHome(path)) {
           validPathsForUI.add(pathSD);
         }
       }
