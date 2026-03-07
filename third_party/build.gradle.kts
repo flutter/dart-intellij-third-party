0f44915ea6d4dbd9c7643051f209a09b17b282d9
javad.asadi <javad.asadi@flix.com>
2026-03-07 13:25:29 +0100

    Add migration files

--- third_party/build.gradle.kts
+++ third_party/build.gradle.kts
@@ -110,6 +110,8 @@ dependencies {
     }
 
     implementation(fileTree("lib") { include("*.jar") })
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.23.1")
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j.jsonrpc:0.23.1")
 
     testImplementation("junit:junit:4.13.2")
 }
