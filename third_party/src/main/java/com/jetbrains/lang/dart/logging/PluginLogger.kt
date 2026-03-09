cc38f572a74951359a137318b7188eece4801296
Helin Shiah <helinx@google.com>
2026-03-09 15:10:05 -0700

    Synchronize on shared log manager

--- third_party/src/main/java/com/jetbrains/lang/dart/logging/PluginLogger.kt
+++ third_party/src/main/java/com/jetbrains/lang/dart/logging/PluginLogger.kt
@@ -20,35 +20,37 @@ object PluginLogger {
     val logPath = PathManager.getLogPath()
     val fullPath = logPath + File.separatorChar + LOG_FILE_NAME
 
-    val flutterLogger = Logger.getLogger("io.flutter")
-    
-    // Check if either logger already has a FileHandler for dash.log
-    val existingHandler = rootLogger.handlers.filterIsInstance<FileHandler>().firstOrNull()
-      ?: flutterLogger.handlers.filterIsInstance<FileHandler>().firstOrNull()
+    synchronized(java.util.logging.LogManager.getLogManager()) {
+      val flutterLogger = Logger.getLogger("io.flutter")
 
-    if (existingHandler != null) {
-      // Another plugin initialized first; reuse its handler
-      if (!rootLogger.handlers.contains(existingHandler)) {
-        rootLogger.addHandler(existingHandler)
-      }
-      if (!flutterLogger.handlers.contains(existingHandler)) {
-        flutterLogger.addHandler(existingHandler)
-      }
-    } else {
-      // We are the first plugin to initialize; create the handler
-      try {
-        val newHandler = FileHandler(fullPath, 10 * 1024 * 1024, 5, true)
-        System.setProperty(
-          "java.util.logging.SimpleFormatter.format",
-          "%1\$tF %1\$tT %3\$s [%4$-7s] %5\$s %6\$s %n"
-        )
-        newHandler.formatter = java.util.logging.SimpleFormatter()
+      // Check if either logger already has a FileHandler for dash.log
+      val existingHandler = rootLogger.handlers.filterIsInstance<FileHandler>().firstOrNull()
+        ?: flutterLogger.handlers.filterIsInstance<FileHandler>().firstOrNull()
 
-        // Attach to both loggers so the next plugin finds it
-        rootLogger.addHandler(newHandler)
-        flutterLogger.addHandler(newHandler)
-      } catch (e: Exception) {
-        throw RuntimeException(e)
+      if (existingHandler != null) {
+        // Another plugin initialized first; reuse its handler
+        if (!rootLogger.handlers.contains(existingHandler)) {
+          rootLogger.addHandler(existingHandler)
+        }
+        if (!flutterLogger.handlers.contains(existingHandler)) {
+          flutterLogger.addHandler(existingHandler)
+        }
+      } else {
+        // We are the first plugin to initialize; create the handler
+        try {
+          val newHandler = FileHandler(fullPath, 10 * 1024 * 1024, 5, true)
+          System.setProperty(
+            "java.util.logging.SimpleFormatter.format",
+            "%1\$tF %1\$tT %3\$s [%4$-7s] %5\$s %6\$s %n"
+          )
+          newHandler.formatter = java.util.logging.SimpleFormatter()
+
+          // Attach to both loggers so the next plugin finds it
+          rootLogger.addHandler(newHandler)
+          flutterLogger.addHandler(newHandler)
+        } catch (e: Exception) {
+          throw RuntimeException(e)
+        }
       }
     }
   }
