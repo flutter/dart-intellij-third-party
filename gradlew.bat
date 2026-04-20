6f22e6198d1a9dd2abd8e7d0bed8220d9a149521
pq <pquitslund@google.com>
2026-04-20 10:34:19 -0700

    fix

--- gradlew.bat
+++ gradlew.bat
@@ -2,5 +2,4 @@
 :: NOTE: Any file paths passed as arguments should be relative to the 'third_party' 
 :: directory (or fully qualified).
 
-cd /d "%~dp0third_party"
-call gradlew.bat %*
+call "%~dp0third_party\gradlew.bat" --project-dir "%~dp0third_party" %*
