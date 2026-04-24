7826037502425281216e1da9aa96f9270622b0b9
Helin Shiah <helinx@google.com>
2026-04-24 11:50:27 -0700

    Use MessageProducer to send responses

--- third_party/src/main/java/com/jetbrains/lang/dart/lsp/DartVirtualStreamConnectionProvider.kt
+++ third_party/src/main/java/com/jetbrains/lang/dart/lsp/DartVirtualStreamConnectionProvider.kt
@@ -1,12 +1,12 @@
-package com.jetbrains.lang.dart.analyzer
+package com.jetbrains.lang.dart.lsp
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService
 import com.jetbrains.lang.dart.logging.PluginLogger
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider
-import org.eclipse.lsp4j.CompletionOptions
 import org.eclipse.lsp4j.InitializeResult
 import org.eclipse.lsp4j.ServerCapabilities
 import org.eclipse.lsp4j.jsonrpc.json.MessageJsonHandler
@@ -18,7 +18,6 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
-import java.nio.charset.StandardCharsets
 
 class DartVirtualStreamConnectionProvider(private val project: Project) : StreamConnectionProvider {
     companion object {
@@ -28,11 +27,11 @@ class DartVirtualStreamConnectionProvider(private val project: Project) : Stream
 
     // Stream for writing LSP responses from the virtual server to the lsp4ij client.
     // I'm not even sure we need this at all.
-    val clientLspInputStream = PipedInputStream(1024*1024)
+    val clientLspInputStream = PipedInputStream(1024 * 1024)
     val virtualServerLspOutputStream = PipedOutputStream()
 
     // Stream for the lsp4ij client to write requests to the virtual server.
-    val virtualServerLspInputStream = PipedInputStream(1024*1024)
+    val virtualServerLspInputStream = PipedInputStream(1024 * 1024)
     val clientLspOutputStream = PipedOutputStream()
 
     init {
@@ -40,10 +39,51 @@ class DartVirtualStreamConnectionProvider(private val project: Project) : Stream
         virtualServerLspInputStream.connect(clientLspOutputStream)
     }
 
+    // This stores IDs that have been used by lsp4ij for LSP-over-legacy requests to DAS.
+    // There are non-lsp4ij requests that are sent as LSP-over-legacy, but we don't need to forward responses for those
+    // requests to lsp4ij since lsp4ij was not the originator.
+    private val pendingLegacyIds = java.util.concurrent.ConcurrentHashMap.newKeySet<String>()
+
     override fun start() {
         logger.info("Starting DartVirtualStreamConnectionProvider")
         println("Starting DartVirtualStreamConnectionProvider")
+
+        val dartAnalysisService = DartAnalysisServerService.getInstance(project)
+
         ApplicationManager.getApplication().executeOnPooledThread {
+            // Listen for analysis server responses.
+            dartAnalysisService.addResponseListener { response ->
+                val jsonObject = JsonParser.parseString(response).asJsonObject
+
+                var lspPayload: JsonObject? = null
+
+                // Try to extract a message.
+                if (jsonObject.has("params")) {
+                    val params = jsonObject.get("params").asJsonObject
+                    if (params.has("lspMessage")) {
+                        lspPayload = params.get("lspMessage").asJsonObject
+                    }
+                }
+
+                // Try to extract a response.
+                if (lspPayload == null && jsonObject.has("result")) {
+                    val topLevelId = if (jsonObject.has("id")) jsonObject.get("id").asString else null
+                    if (topLevelId != null && pendingLegacyIds.remove(topLevelId)) {
+                        val result = jsonObject.get("result").asJsonObject
+                        if (result.has("lspResponse")) {
+                            lspPayload = result.get("lspResponse").asJsonObject
+                        }
+                    }
+                }
+
+                if (lspPayload != null) {
+                    val producer = DartMessageProducer.getProducer(project)
+                    producer?.enqueueResponse(lspPayload.toString())
+                }
+            }
+
+            println("Finished setting up DAS listening")
+
             // Listen for lsp4ij messages.
             val requestReader = StreamMessageProducer(virtualServerLspInputStream, JSON_HANDLER)
             requestReader.listen { message ->
@@ -54,7 +94,7 @@ class DartVirtualStreamConnectionProvider(private val project: Project) : Stream
                         "initialize" -> {
                             // Write a response to lsp4ij pretending to initialize a server.
                             val capabilities = ServerCapabilities()
-                            capabilities.setHoverProvider(true)
+//                            capabilities.setHoverProvider(true)
 
                             val initResult = InitializeResult(capabilities)
 
@@ -69,7 +109,20 @@ class DartVirtualStreamConnectionProvider(private val project: Project) : Stream
                             // Write a response to lsp4ij
                         }
                         "textDocument/hover" -> {
+                            logger.info("Hover message received: $message")
+                            val legacyRequest = JsonObject()
+                            val legacyId = dartAnalysisService.generateUniqueId()
+                            pendingLegacyIds.add(legacyId)
+                            legacyRequest.addProperty("id", legacyId)
+                            legacyRequest.addProperty("method", "lsp.handle")
+
+                            val params = JsonObject()
+                            params.add("lspMessage", JSON_HANDLER.gson.toJsonTree(message).asJsonObject)
+                            legacyRequest.add("params", params)
+
+                            println("hover request: $legacyRequest")
                             // Forward to DAS.
+                            dartAnalysisService.sendRequest(legacyId, legacyRequest)
                         }
                         else -> {
                             logger.info("Ignored unimplemented method from lsp4ij request: $method")
@@ -81,44 +134,24 @@ class DartVirtualStreamConnectionProvider(private val project: Project) : Stream
                     logger.info("Ignored unrecognized message type from lsp4ij request: $message")
                 }
             }
-
-            // Listen for analysis server responses.
-            DartAnalysisServerService.getInstance(project).addResponseListener { response ->
-                println("DAS response: $response")
-
-                val jsonObject = JsonParser.parseString(response).asJsonObject
-
-                var lspPayload: JsonObject? = null
-
-                // Try to extract a message.
-                if (jsonObject.has("params")) {
-                    val params = jsonObject.get("params").asJsonObject
-                    if (params.has("lspMessage")) {
-                        lspPayload = params.get("lspMessage").asJsonObject
-                    }
-                }
-
-                // Try to extract a response.
-                if (lspPayload != null && jsonObject.has("payload")) {
-                    val result = jsonObject.get("result").asJsonObject
-                    if (result.has("lspResponse")) {
-                        lspPayload = result.get("lspResponse").asJsonObject
-                    }
-                }
-
-                // Send to stream
-            }
         }
     }
 
     private fun sendResponseToClient(response: ResponseMessage) {
         val jsonString = JSON_HANDLER.gson.toJson(response)
         println("Sending response: $jsonString")
-        val body: ByteArray = jsonString.encodeToByteArray()
-        val header = "Content-Length: " + body.size + "\r\n\r\n";
-        virtualServerLspOutputStream.write(header.encodeToByteArray())
-        virtualServerLspOutputStream.write(body)
-        virtualServerLspOutputStream.flush()
+
+        enqueueResponse(jsonString)
+    }
+
+    private fun enqueueResponse(jsonString: String) {
+        val producer = DartMessageProducer.getProducer(project)
+        if (producer == null) {
+            logger.warn("DartMessageProducer does not have a registered producer for project: ${project.name}")
+            return
+        }
+
+        producer.enqueueResponse(jsonString)
     }
 
     override fun getInputStream(): InputStream? {
