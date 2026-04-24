b8e641b58279ce5b369f4be8e6f6aef728b41a6b
Helin Shiah <helinx@google.com>
2026-04-24 12:58:21 -0700

    Add comments for new classes

--- third_party/src/main/java/com/jetbrains/lang/dart/lsp/DartLspClientFeatures.kt
+++ third_party/src/main/java/com/jetbrains/lang/dart/lsp/DartLspClientFeatures.kt
@@ -9,6 +9,14 @@ import com.redhat.devtools.lsp4ij.client.features.LSPClientFeatures
 import org.eclipse.lsp4j.jsonrpc.Launcher
 import org.eclipse.lsp4j.services.LanguageServer
 
+/**
+ * Custom LSP client features for Dart.
+ *
+ * This class extends [LSPClientFeatures] to customize the LSP connection lifecycle.
+ * It provides a [DartLauncherBuilder] to override the default message production,
+ * allowing messages to be bridged directly from the Dart Analysis Server (DAS)
+ * instead of relying solely on standard input streams.
+ */
 class DartLspClientFeatures : LSPClientFeatures() {
     override fun <S : LanguageServer> createLauncherBuilder(): Launcher.Builder<S> {
         return DartLauncherBuilder<S>(this)
