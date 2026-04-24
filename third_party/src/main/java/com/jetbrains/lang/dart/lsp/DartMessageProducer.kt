8e5617e7fe290d283dceb099e26186dea5b7317f
Helin Shiah <helinx@google.com>
2026-04-24 12:17:29 -0700

    Stop server nicely and catch pipe broken exception

--- third_party/src/main/java/com/jetbrains/lang/dart/lsp/DartMessageProducer.kt
+++ third_party/src/main/java/com/jetbrains/lang/dart/lsp/DartMessageProducer.kt
@@ -28,12 +28,18 @@ class DartMessageProducer(val jsonHandler: MessageJsonHandler) : MessageProducer
         fun unregisterProducer(project: Project) {
             projectProducers.remove(project)
         }
+        private const val POISON_PILL = "SHUTDOWN_POISON_PILL"
     }
 
     // This is queue of responses from the DAS.
     val responseQueue = LinkedBlockingQueue<String>()
 
     var keepRunning = true;
+
+    fun stop() {
+        keepRunning = false
+        responseQueue.offer(POISON_PILL)
+    }
 
     // Enqueue an LSP message to be forwarded to lsp4ij (from the DAS or a virtual representation of the DAS).
     fun enqueueResponse(json: String) {
@@ -43,6 +49,7 @@ class DartMessageProducer(val jsonHandler: MessageJsonHandler) : MessageProducer
     override fun listen(messageConsumer: MessageConsumer?) {
         while (messageConsumer != null && keepRunning) {
             val json = responseQueue.take()
+            if (json == POISON_PILL) break
 
             try {
                 val message = jsonHandler.parseMessage(json)
