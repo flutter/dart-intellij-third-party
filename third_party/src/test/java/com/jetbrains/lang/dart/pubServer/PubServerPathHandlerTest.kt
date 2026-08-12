8785a1e0a99dca063c881767809f36d9b696ad27
Juan Mora <300735171+juan-mora-google@users.noreply.github.com>
2026-08-12 18:15:32 +0000

    Remove license line

--- third_party/src/test/java/com/jetbrains/lang/dart/pubServer/PubServerPathHandlerTest.kt
+++ third_party/src/test/java/com/jetbrains/lang/dart/pubServer/PubServerPathHandlerTest.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.lang.dart.pubServer
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
