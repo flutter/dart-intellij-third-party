51f8945723906f8326b52905a261f314aa7ea215
javad.asadi <javadasadipc2015@gmail.com>
2026-08-14 20:50:39 +0200

    Move VmService files from /thirdPartySrc to src

--- third_party/src/main/java/com/jetbrains/lang/dart/vm/service/internal/BlockingRequestSink.java
+++ third_party/src/main/java/com/jetbrains/lang/dart/vm/service/internal/BlockingRequestSink.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.dartlang.vm.service.internal;
+package com.jetbrains.lang.dart.vm.service.internal;
 
 import com.google.common.collect.Lists;
 import com.google.gson.JsonObject;
