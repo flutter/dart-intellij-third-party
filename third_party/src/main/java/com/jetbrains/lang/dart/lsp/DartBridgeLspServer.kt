be959389270b149b3d610bd6556dda3ab60495d6
Helin Shiah <helinx@google.com>
2026-09-02 14:08:23 -0700

    Fix deadlock in willRenameFiles by avoiding background runReadAction

--- third_party/src/main/java/com/jetbrains/lang/dart/lsp/DartBridgeLspServer.kt
+++ third_party/src/main/java/com/jetbrains/lang/dart/lsp/DartBridgeLspServer.kt
@@ -428,7 +428,9 @@ class DartBridgeLspServer(private val project: Project) : DartLanguageServer, Te
             return future
         }
 
-        das.updateFilesContent()
+        if (com.intellij.openapi.application.ApplicationManager.getApplication().isReadAccessAllowed) {
+            das.updateFilesContent()
+        }
 
         val legacyId = das.generateUniqueId()
         if (legacyId == null) {
