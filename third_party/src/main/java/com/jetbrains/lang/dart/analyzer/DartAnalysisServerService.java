00c69dd18a4ea984c6b028ebbe12a3a23c09f86c
Helin Shiah <helinx@google.com>
2026-09-11 21:25:56 -0700

    Fix applyEdit response handling and remove unused resolveCodeAction

--- third_party/src/main/java/com/jetbrains/lang/dart/analyzer/DartAnalysisServerService.java
+++ third_party/src/main/java/com/jetbrains/lang/dart/analyzer/DartAnalysisServerService.java
@@ -2836,6 +2836,13 @@ public final class DartAnalysisServerService implements Disposable {
     }
   }
 
+  public void sendResponse(JsonObject response) {
+    final RemoteAnalysisServerImpl server = myServer;
+    if (server != null) {
+      server.sendResponseToServer(response);
+    }
+  }
+
   /**
    * Send the request and associate it with the passed {@link com.google.dart.server.Consumer}.
    */
